ed4ffdb1397b5fc17952c3331edf02a94fa277a9
IsakEd <106615703+IsakEd@users.noreply.github.com>
2023-05-23 21:37:04 +0200

    added svelte store to astro somehow

---
+++ src/components/Todos.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Todo from "./Todo.svelte";
+    import { todos } from "../store";
+</script>
+
+<div class="container">
+    {#each $todos as todo}
+        <Todo {...todo} />
+    {/each}
+</div>
+
+<style>
+    .container {
+        margin: 1em;
+    }
+</style>
