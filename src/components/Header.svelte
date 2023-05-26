2d8f083ad42f06654a64306c8708f2abea2bef79
IsakEd <isak.ederlov@gmail.com>
2023-05-26 13:30:56 +0200

    added modal settings in Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,15 +1,24 @@
 <script>
-
+	import Modal from "../lib/Modal.svelte";
 	let showModal = false;
+	const reset = () => {
+		localStorage.clear();
+		location.reload();
+	}
 </script>
 
 <div class="header">
 	<button class="invisible"> <img class="header-icon" src="/menu.svg" alt="menu" /> </button>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<h1>WA project</h1>
-	<button class="invisible"> <img class="header-icon" src="/settings.svg" alt="menu" /> </button>
+	<button on:click={()=>showModal=true} class="invisible"> <img class="header-icon" src="/settings.svg" alt="menu" /> </button>
 
 </div>
+
+<Modal bind:showModal>
+	<h2>Settings</h2>
+	<button on:click={reset}>clear all data</button>
+</Modal>
 
 <style>
 	h1 {
