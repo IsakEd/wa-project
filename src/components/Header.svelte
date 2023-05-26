37ee6b35b29115c86848e60bc519f965d7cffc1a
Isak Ederlöv <nope@gmail.com>
2023-05-27 01:01:33 +0200

    fixed header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,46 +1,56 @@
 <script>
-	import Modal from "../lib/Modal.svelte";
-	let showModal = false;
-	const reset = () => {
-		localStorage.clear();
-		location.reload();
-	}
+  import Modal from "../lib/Modal.svelte";
+
+  let showModal = false;
+  $: showNav = false;
+  const reset = () => {
+    localStorage.clear();
+    location.reload();
+  };
 </script>
 
 <div class="header">
-	<button class="invisible"> <img class="header-icon" src="/menu.svg" alt="menu" /> </button>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<h1>WA project</h1>
-	<button on:click={()=>showModal=true} class="invisible"> <img class="header-icon" src="/settings.svg" alt="menu" /> </button>
-
+  <button on:click={() => (showNav = !showNav)} class="invisible">
+    <img class="header-icon" src="/menu.svg" alt="menu" />
+  </button>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  {#if showNav}
+    <h1><a href="/todo-page">Todos</a></h1>
+    <h1><a href="/goals">Goals</a></h1>
+  {:else}
+    <h1>WA task</h1>
+  {/if}
+  <button on:click={() => (showModal = true)} class="invisible">
+    <img class="header-icon" src="/settings.svg" alt="settings" />
+  </button>
 </div>
 
 <Modal bind:showModal>
-	<h2>Settings</h2>
-	<button on:click={reset}>clear all data</button>
+  <h2>Settings</h2>
+  <button on:click={reset}>clear all data</button>
 </Modal>
 
 <style>
-	h1 {
-		font-size: larger;
-		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-		font-weight: bold;
-		flex: 1;
-		text-align: center;
-	}
+  h1 {
+    font-size: larger;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: bold;
+    flex: 1;
+    text-align: center;
+  }
 
-	.header {
-		padding: 0 0.5rem;
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		background-color: var(--secondary-color);
-		border-bottom: 1px solid var(--contrast-color);
-	}
+  .header {
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: var(--secondary-color);
+    border-bottom: 1px solid var(--contrast-color);
+  }
 
-	.header-icon {
-		height: 2em;
-		width: 2em;
-		stroke: black;
-	}
+  .header-icon {
+    height: 2em;
+    width: 2em;
+    stroke: black;
+  }
 </style>
